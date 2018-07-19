e5667308119e2e4011b50233f60a563de50409b0
emeric <emeric.lebbrecht@gmail.com>
2018-07-19 17:35:56 +0200

    first controller

---
+++ src/Controller/BilletterieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BilletterieController extends Controller
+{
+    /**
+     * @Route("/billetterie", name="billetterie")
+     */
+    public function index()
+    {
+        return $this->render('billetterie/index.html.twig', [
+            'controller_name' => 'BilletterieController',
+        ]);
+    }
+}
